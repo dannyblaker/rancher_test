03601b2be4430392e0383c8394e4e064611e2e77
Danny Blaker <dannyb@semsci.com>
2023-02-22 12:13:17 +1100

    init
    Signed-off-by: Danny Blaker <dannyb@semsci.com>

---
+++ README.md
@@ -0,0 +1,29 @@
+**Task**
+
+Can you make the modifications required to this repository to deploy the following apps (mapped to the URL provided) to Rancher:
+
+APP: Directus (<https://docs.directus.io/self-hosted/docker-guide.html>)
+URL: your.domain.com/directus
+
+APP: Matamo (<https://github.com/matomo-org/docker/>)
+URL: your.domain.com/matamo
+
+kindly document your changes, and the steps you took, in this README.md file.
+
+All setup instructions are provided below:
+
+Many Thanks!
+
+
+**Task pre-requisites**
+To complete this task you will need:
+ - A free AWS account
+ - A spare domain that you own which you can use temporarily while you work on this job
+ - VScode, setup for remote development (guide here <https://code.visualstudio.com/docs/remote/ssh#_getting-started>)
+
+
+**Follow setup steps in this doc**
+
+
+<https://docs.google.com/document/d/1RVttdiJbjKUF5VxSbwBlsIRe1ZIR2FhNpTavMlZqyj8/edit?usp=sharing>
+
---
+++ setup.sh
@@ -0,0 +1,179 @@
+#!/bin/bash
+
+# Edit these manually
+DOMAIN="your.domain.com"
+EMAIL="test@test.com"
+# the rest is automatic :)
+
+#INSTALLING DOCKER AND DOCKER COMPOSE
+# Stop and remove all containers
+docker compose down
+docker stop $(docker ps -aq)
+docker rm $(docker ps -aq)
+
+# Remove all volumes
+docker volume rm $(docker volume ls -q)
+
+# Remove all networks
+docker network rm $(docker network ls -q)
+
+# Remove all images
+docker rmi $(docker images -q)
+
+# Remove any existing Docker installations
+sudo apt-get remove -y docker docker-engine docker.io containerd runc
+
+# Install prerequisites
+sudo apt-get update
+sudo apt-get install -y \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
+
+# Download and create the GPG key
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+ $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list >/dev/null
+
+# Install Docker
+sudo apt-get update
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+# Install Docker Compose
+sudo curl -L "https://github.com/docker/compose/releases/download/latest/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+# Add the current user to the Docker group
+sudo usermod -aG docker $USER
+
+#DEFINE USEFUL VARIABLES
+# Get the directory that this script is located in
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+# create cert required for Rancher
+
+# Set variables
+CERT_DIR="$SCRIPT_DIR/cert"
+CERT_NAME="my-cert"
+DAYS="90"
+PASSPHRASE=""
+CRON_RENEW_SCHEDULE="0 0 * * *"
+
+# Remove existing certificate directory if exists and create a new one
+sudo rm -rf $CERT_DIR
+mkdir $CERT_DIR
+
+# Generate private key
+openssl genrsa -aes256 -passout pass:$PASSPHRASE -out $CERT_DIR/$CERT_NAME.key 2048
+
+# Generate CSR
+openssl req -new -key $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/$CERT_NAME.csr -subj "/C=AU/ST=SA/L=Adelaide/O=Sintelix/CN=$DOMAIN"
+
+# Generate self-signed certificate
+openssl x509 -req -days $DAYS -in $CERT_DIR/$CERT_NAME.csr -signkey $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/$CERT_NAME.crt -passin pass:$PASSPHRASE
+
+# Remove passphrase from private key
+openssl rsa -in $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/$CERT_NAME.key -passin pass:$PASSPHRASE
+
+# Concatenate certificate and CA certificate into full chain
+cat $CERT_DIR/$CERT_NAME.crt $CERT_DIR/$CERT_NAME.crt >$CERT_DIR/FULL_CHAIN.pem
+
+# Extract private key
+openssl rsa -in $CERT_DIR/$CERT_NAME.key -out $CERT_DIR/PRIVATE_KEY.pem -passin pass:$PASSPHRASE
+
+# Extract CA certificates
+openssl x509 -in $CERT_DIR/$CERT_NAME.crt -text | grep "Issuer" | awk '{print $3}' | sort -u >$CERT_DIR/CA_CERTS.pem
+
+# Set permissions on private key
+chmod 400 $CERT_DIR/PRIVATE_KEY.pem
+
+# Add a cron job to renew the certificate
+echo "# Renew the SSL certificate one week before it expires" >$CERT_DIR/cronjob
+echo "$CRON_RENEW_SCHEDULE certbot renew --pre-hook 'service nginx stop' --post-hook 'service nginx start' --deploy-hook 'cat $CERT_DIR/privkey.pem $CERT_DIR/fullchain.pem > /etc/letsencrypt/live/$DOMAIN/privkey.pem; service nginx restart'" >>$CERT_DIR/cronjob
+echo "" >>$CERT_DIR/cronjob
+
+# Add cron job to the crontab
+crontab $CERT_DIR/cronjob
+
+echo "Certificate generated and cron job set up for automatic renewal."
+
+#----------------------------------------------------------
+#DEPLOYING RANCHER
+# Set the path to the Docker Compose file for Rancher
+DOCKER_COMPOSE_RANCHER="$SCRIPT_DIR/rancher/docker-compose.yml"
+
+# Run Docker Compose for Rancher in the specified directory
+docker compose -f "$DOCKER_COMPOSE_RANCHER" up -d
+
+#SETTING UP DIRECTUS
+# Set the path to the Docker Compose file for Directus
+DOCKER_COMPOSE_DIRECTUS="$SCRIPT_DIR/directus/docker-compose.yml"
+
+# create a .env file for Directus...
+DIRECTUS_ENV = "$SCRIPT_DIR/directus/.env"
+# Remove read-only attribute if .env file already exists
+if [ -f DIRECTUS_ENV ]; then
+  chmod +w DIRECTUS_ENV
+fi
+
+DIRECTUS_PG_PASS=$(openssl rand -hex 16)
+DIRECTUS_ADMIN_PASS=$(openssl rand -hex 16)
+DIRECTUS_ADMIN_EMAIL="$EMAIL"
+KEY=$(uuidgen)
+SECRET=$(uuidgen)
+PUBLIC_URL="$DOMAIN"
+
+cat >directus/.env <<EOF
+DIRECTUS_PG_PASS=$DIRECTUS_PG_PASS
+DIRECTUS_ADMIN_PASS=$DIRECTUS_ADMIN_PASS
+DIRECTUS_ADMIN_EMAIL=$DIRECTUS_ADMIN_EMAIL
+KEY=$KEY
+SECRET=$SECRET
+PUBLIC_URL=$PUBLIC_URL
+EOF
+
+echo "The .env file has been created in the 'directus' directory."
+
+if [ -f DIRECTUS_ENV ]; then
+  chmod -w DIRECTUS_ENV
+fi
+
+# create a .env file for useful variables...
+USEFUL_VARS_ENV = "$SCRIPT_DIR/.env"
+# Remove read-only attribute if .env file already exists
+if [ -f USEFUL_VARS_ENV ]; then
+  chmod +w USEFUL_VARS_ENV
+fi
+
+DOMAIN="your.domain.com"
+EMAIL="test@test.com"
+DOCKER_COMPOSE_CYBERCHEF="$SCRIPT_DIR/cyberchef/docker-compose.yml"
+DOCKER_COMPOSE_DIRECTUS="$SCRIPT_DIR/directus/docker-compose.yml"
+
+cat >/.env <<EOF
+DOMAIN=$DOMAIN
+EMAIL=$EMAIL
+DOCKER_COMPOSE_CYBERCHEF=$DOCKER_COMPOSE_CYBERCHEF
+DOCKER_COMPOSE_DIRECTUS=$DOCKER_COMPOSE_DIRECTUS
+EOF
+
+echo "The .env file has been created in the 'directus' directory."
+
+if [ -f USEFUL_VARS_ENV ]; then
+  chmod -w USEFUL_VARS_ENV
+fi
+
+# now over to you :D
+
+# can you make the modifications required to this
+# repository to deploy the following apps to Rancher:
+
+#   - Directus to your.domain.com/directus
+#   - Cyberchef to your.domain.com/cyberchef
+
+# kindly document your changes in the README.md file
